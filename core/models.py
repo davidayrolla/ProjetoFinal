ab31bc956150ca3c4367d5d30836117f0aedd5fe
David Ayrolla <davidayrolla@gmail.com>
2023-02-12 10:31:00 -0300

    Tirando Pessoa.observacoes

--- core/models.py
+++ core/models.py
@@ -14,7 +14,7 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=200,blank=True,null=True)
     telefone = models.CharField(max_length=20,blank=True,null=True)
-    documento = models.CharField(max_length=20,blank=True,null=True)
+    # documento = models.CharField(max_length=20,blank=True,null=True)
 
     def __str__(self):
         return self.nome
