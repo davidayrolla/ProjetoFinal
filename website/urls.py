1684491d32af547901fa02ea1d55c9744b995c23
David Ayrolla <davidayrolla@gmail.com>
2023-02-16 19:48:45 -0300

    Página de contato

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path, include
-from .views import home
+from .views import home, contato
 
 urlpatterns = [
     re_path(r'^$', home, name='website_home'),
+    re_path(r'home', home, name='website_home'),
+    re_path(r'contato', contato, name='website_contato'),
 ]
