9ff62c55b9a76462bbb30a79b6e756e791495f5e
David Ayrolla <davidayrolla@gmail.com>
2023-02-17 09:12:38 -0300

    Páginas desenhadas.

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, re_path, include
-from .views import home, contato
+from .views import home, contato, servicos, sobre, planos
 
 urlpatterns = [
     re_path(r'^$', home, name='website_home'),
     re_path(r'home', home, name='website_home'),
     re_path(r'contato', contato, name='website_contato'),
+    re_path(r'servicos', servicos, name='website_servicos'),
+    re_path(r'sobre', sobre, name='website_sobre'),
+    re_path(r'planos', planos, name='website_planos'),
 ]
--- website/views.py
+++ website/views.py
@@ -6,3 +6,18 @@ def home(request):
 
 def contato(request):
     return render(request, 'website/contato.html')
+
+
+def servicos(request):
+    return render(request, 'website/servicos.html')
+
+
+def sobre(request):
+    return render(request, 'website/sobre.html')
+
+
+def planos(request):
+    return render(request, 'website/planos.html')
+
+
+
