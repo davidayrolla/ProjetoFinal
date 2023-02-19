ad068d1c741170973d5bb28c77ea3fe0b6f37bd0
David Ayrolla <davidayrolla@gmail.com>
2023-02-19 11:31:22 -0300

    Tirando funcionalidade do PDF por estar dando erro no Heroku.

--- core/views.py
+++ core/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Pessoa, Veiculo, MovimentoRotativo, MovimentoMensal, Mensalista
 from .forms import PessoaForm, VeiculoForm, MovimentoRotativoForm, MovimentoMensalForm, MensalistaForm
 from django.http import HttpResponse
-from django.template.loader import get_template
-import xhtml2pdf.pisa as pisa
-import io
+# from django.template.loader import get_template
+# import xhtml2pdf.pisa as pisa
+# import io
 from django.views.generic.base import View
 import csv
 
@@ -225,32 +225,32 @@ def mensalista_delete(request, id):
         return render(request, 'core/delete_confirm.html', {'obj': mensalista})
 
 
-class Render:
-    @staticmethod
-    def render(path: str, params: dict, filename: str):
-        template = get_template(path)
-        html = template.render(params)
-        response = io.BytesIO()
-        pdf = pisa.pisaDocument(
-            io.BytesIO(html.encode("UTF-8")), response)
-        if not pdf.err:
-            response = HttpResponse(
-                response.getvalue(), content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment;filename=%s.pdf' % filename
-            return response
-        else:
-            return HttpResponse("Error Rendering PDF", status=400)
+# class Render:
+#     @staticmethod
+#     def render(path: str, params: dict, filename: str):
+#         template = get_template(path)
+#         html = template.render(params)
+#         response = io.BytesIO()
+#         pdf = pisa.pisaDocument(
+#             io.BytesIO(html.encode("UTF-8")), response)
+#         if not pdf.err:
+#             response = HttpResponse(
+#                 response.getvalue(), content_type='application/pdf')
+#             response['Content-Disposition'] = 'attachment;filename=%s.pdf' % filename
+#             return response
+#         else:
+#             return HttpResponse("Error Rendering PDF", status=400)
 
 
-class Pdf(View):
-
-    def get(self, request):
-        veiculos = Veiculo.objects.all()
-        params = {
-            'veiculos': veiculos,
-            'request': request,
-        }
-        return Render.render( 'core/relatorio.html', params, 'relatorios_veiculos')
+# class Pdf(View):
+#
+#     def get(self, request):
+#         veiculos = Veiculo.objects.all()
+#         params = {
+#             'veiculos': veiculos,
+#             'request': request,
+#         }
+#         return Render.render( 'core/relatorio.html', params, 'relatorios_veiculos')
 
 
 class ExportarParaCSV(View):
